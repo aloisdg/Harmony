e8ec401f99cbd64fc7d66878273f6a1d21e3be5f
aloisdg <aloisdegouvello@live.fr>
2016-06-12 14:47:39 +0200

    feat(lib): Add missing features. Related to #1

--- Harmony/Harmony.cs
+++ Harmony/Harmony.cs
@@ -12,31 +12,11 @@ using ColorMine.ColorSpaces;
 // https://duckduckgo.com/?q=color+picker+%23c715f0&ia=colorpicker
 // http://www.sessions.edu/color-calculator
 
+// Others
+// http://programmers.stackexchange.com/questions/44929/color-schemes-generation-theory-and-algorithms
+
 namespace Harmony {
     public class Harmony {
-        // http://galacticmilk.com/sphere/#config=%7B%22hsl%22:%7B%22H%22:360,%22S%22:100,%22L%22:0%7D,%22theme%22:%22dark%22,%22quantize%22:0,%22harmony%22:%22Complementary%22,%22vision%22:%22Normal%22,%22space%22:%22RYB%22,%22top%22:62,%22left%22:50,%22diameter%22:500,%22scale%22:1,%22dot%22:false%7D
-        //private readonly Dictionary<string, int[]> _harmonies = new Dictionary<string, int[]>
-        //{
-        //    { "complementary", new[] {0, 180}},
-        //    { "splitComplementary", new[] {0, 150, 320}},
-        //    { "splitComplementaryCW", new[] {0, 150, 300}},
-        //    { "splitComplementaryCCW", new[] {0, 60, 210}},
-        //    { "triadic", new[] {0, 120, 240}},
-        //    { "clash", new[] {0, 90, 270}},
-        //    { "tetradic", new[] {0, 90, 180, 270}},
-        //    { "fourToneCW", new[] {0, 60, 180, 240}},
-        //    { "fourToneCCW", new[] {0, 120, 180, 300}},
-        //    { "fiveToneA", new[] {0, 115, 155, 205, 245}},
-        //    { "fiveToneB", new[] {0, 40, 90, 130, 245}},
-        //    { "fiveToneC", new[] {0, 50, 90, 205, 320}},
-        //    { "fiveToneD", new[] {0, 40, 155, 270, 310}},
-        //    { "fiveToneE", new[] {0, 115, 230, 270, 320}},
-        //    { "sixToneCW", new[] {0, 30, 120, 150, 240, 270}},
-        //    { "sixToneCCW", new[] {0, 90, 120, 210, 240, 330}},
-        //    { "neutral", new[] {0, 15, 30, 45, 60, 75}},
-        //    {"analogous", new[] {0, 30, 60, 90, 120, 150}}
-        //};
-
         // http://www.tigercolor.com/color-lab/color-theory/color-harmonies.htm
         // http://www.sensationalcolor.com/understanding-color/theory/color-relationships-creating-color-harmony-1849
         // http://www.chainstyle.com/tutorials/colscheme.html
@@ -44,15 +24,15 @@ namespace Harmony {
         private readonly Dictionary<string, int[]> _harmonies = new Dictionary<string, int[]> {
             { "complementary", new[] { 0, 6 }},
             { "splitComplementary", new[] { 0, 5, 7 }},
-            //{ "doubleComplementaryRight", new[] { 0, 1, 6, 7 }},
-            //{ "doubleComplementaryLeft", new[] { 0, 5, 6, 11 }},
+            { "doubleComplementaryRight", new[] { 0, 1, 6, 7 }},
+            { "doubleComplementaryLeft", new[] { 0, 5, 6, 11 }},
             { "triadic", new[] { 0, 4, 8 }},
-            //{ "tetradicRight", new[] { 0, 2, 6, 8 }},
-            //{ "tetradic", new[] { 0, 3, 6, 9 }}, // also called square
+            { "tetradicRight", new[] { 0, 2, 6, 8 }},
+            { "tetradic", new[] { 0, 3, 6, 9 }}, // also called square
             { "tetradicLeft", new[] { 0, 4, 6, 10 }},
             { "analogous", new[] { 0, 1, 11 }},
-            //{ "diadRight", new[] { 0, 2 }},
-            //{ "diadLeft", new[] { 0, 10 }}
+            { "diadRight", new[] { 0, 2 }},
+            { "diadLeft", new[] { 0, 10 }}
         };
 
         private static IEnumerable<Color> Harmonize(Color color, IEnumerable<int> degrees) {
@@ -119,7 +99,8 @@ namespace Harmony {
             Func<double, double, double, double> setBetween
                 = (x, min, max) => x > min ? x < max ? x : max : min;
             Func<int, double> getLightness
-                = x => ((double) x - 3) * 5;
+                = x => ((double) x - 3)*5;
+
             var hsl = color.ToHsl ();
             return Enumerable.Range (0, 7).Select (shade => new Hsl {
                 H = hsl.H,
